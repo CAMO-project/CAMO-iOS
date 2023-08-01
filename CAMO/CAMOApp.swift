50e29e852c0ab4bbf0a9bee4e542f4139fb8ef6e
julia8024 <julia8024@naver.com>
2023-08-01 22:07:50 +0900

    create project

---
+++ CAMO/CAMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAMOApp.swift
+//  CAMO
+//
+//  Created by 장세희 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct CAMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
